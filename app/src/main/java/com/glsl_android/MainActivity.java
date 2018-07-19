e6645b9b358ac4a82a7a0a1ae2dccce8bbc71bb7
Linus-Yang <chyangbast@yeah.net>
2018-07-20 00:09:13 +0800

    test uniform

--- app/src/main/java/com/glsl_android/MainActivity.java
+++ app/src/main/java/com/glsl_android/MainActivity.java
@@ -1,6 +1,8 @@
 package com.glsl_android;
 
 import android.graphics.Shader;
+import android.opengl.EGL14;
+import android.opengl.EGLDisplay;
 import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
@@ -34,18 +36,22 @@ public class MainActivity extends AppCompatActivity {
     private class GLRenderer implements GLSurfaceView.Renderer {
 
        private String mVertexShader = "#version 300 es        \n"
+               +        "  uniform float a_scale;                 \n "
                +        "  layout(location = 0) in vec4 a_position;  \n"
+               +        "  out vec4 o_color;                      \n "
                +        "  void main()                           \n"
                +        " {                                          \n"
-               +        "       gl_Position = a_position;            \n "
+               +        "    gl_Position = vec4(a_position.x * a_scale, a_position.y * a_scale, a_position.z, 1.0); \n "
+               +        "    o_color =  vec4(a_position.x * a_scale, a_position.y * a_scale, a_position.z, 1.0); "
                +        " }                                          \n";
 
        private String mFragmentShader = "#version 300 es            \n "
                +        " precision mediump float;                  \n"
                +       "  layout(location = 0) out vec4 o_fragColor;  \n "
+               +       "  in vec4 o_color;                            \n "
                +       " void main()                                  \n"
                +       " {                                            \n"
-               +       "    o_fragColor  = vec4(1.0, 0.0, 0.0, 1.0);  \n"
+               +       "    o_fragColor  = o_color;                   \n"
                +       " } ";
 
        private int mProgram = -1;
@@ -55,8 +61,9 @@ public class MainActivity extends AppCompatActivity {
                                  1.0f, -1.0f, 0.0f,
                                  0.0f, 1.0f, 0.0f};
 
+       private float scale = 0.0f;
        private FloatBuffer mVertexBuffer;
-
+       private int gScaleLocation;
 
         @Override
         public void onSurfaceCreated(GL10 gl, EGLConfig config) {
@@ -67,11 +74,12 @@ public class MainActivity extends AppCompatActivity {
           int  fragmentShader = ShaderUtil.loadShader(GLES30.GL_FRAGMENT_SHADER, mFragmentShader);
           int program =  ShaderUtil.assembleProgram(vertexShader, fragmentShader);
           mProgram = program;
-         int a = GLES30.glGetAttribLocation(mProgram, "a_position");
-         GLES30.glGenBuffers(1,VBO, 0);
-         GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, VBO[0]);
-         GLES30.glBufferData(GLES30.GL_ARRAY_BUFFER, vector.length * 4, mVertexBuffer, GLES30.GL_STATIC_DRAW);
-         GLES30.glUseProgram(mProgram);
+          GLES30.glGenBuffers(1,VBO, 0);
+          GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, VBO[0]);
+          GLES30.glBufferData(GLES30.GL_ARRAY_BUFFER, vector.length * 4, mVertexBuffer, GLES30.GL_STATIC_DRAW);
+          GLES30.glUseProgram(mProgram);
+
+            gScaleLocation = GLES30.glGetUniformLocation(program, "a_scale");
 
         }
 
@@ -84,6 +92,10 @@ public class MainActivity extends AppCompatActivity {
         public void onDrawFrame(GL10 gl) {
             GLES30.glClearColor(0, 1, 0, 1);
             GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
+
+            scale += 0.01;
+            GLES30.glUniform1f(gScaleLocation, (float) Math.sin(scale));
+
             GLES30.glEnableVertexAttribArray(0);
             GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, VBO[0]);
             GLES30.glVertexAttribPointer ( 0, 3,
